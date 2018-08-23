940ca9db4b7f5093552a47d4ed502c5df3da5011
栗原北斗 <martnight1011@gmail.com>
2018-08-23 12:01:11 +0900

    beaconデータ増やした

--- database/seeds/BeaconsTableSeeder.php
+++ database/seeds/BeaconsTableSeeder.php
@@ -15,6 +15,9 @@ class BeaconsTableSeeder extends Seeder
           ['major' => '3333','minor' => '1', 'position' => 'カフェアイパッド' ],
           ['major' => '3333','minor' => '2', 'position' => 'カフェダーツ' ],
           ['major' => '3333','minor' => '3', 'position' => 'エントランス' ],
+          ['major' => '3333','minor' => '4', 'position' => 'トイレ' ],
+          ['major' => '3333','minor' => '5', 'position' => 'エレベータ' ],
+          ['major' => '3333','minor' => '6', 'position' => 'オフィス' ],
       ]);
     }
 }
